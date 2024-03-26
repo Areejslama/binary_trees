e2033c07075d02cb7660d5c8c565c3f3fb16d7f9
Areejslama <Areegahmed449@gmail.com>
2024-03-26 15:48:21 -0700

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t right_h = 0;
 	size_t left_h = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
@@ -19,8 +20,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			right_h = tree->right ? 1 + binary_tree_height(tree->right): 0;
-			left_h =  tree->left ? 1 + binary_tree_height(tree->left): 0;
+			right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			left_h =  tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		}
 	}
 	return (left_h > right_h ? left_h : right_h);
