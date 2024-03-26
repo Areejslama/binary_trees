d63dd80a1c06eabd9da06a96daae62381245222f
Areejslama <Areegahmed449@gmail.com>
2024-03-26 15:00:04 -0700

    in

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -14,13 +14,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	if (tree->left != NULL)
-	{
-		binary_tree_inorder(tree->left, func);
-	}
-	if (tree->right != NULL)
-	{
-		binary_tree_inorder(tree->right, func);
-	}
+	binary_tree_inorder(tree->right, func);
 }
