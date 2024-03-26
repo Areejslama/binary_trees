96d0da907a886ed9ebf78e729d5f5de029bcabae
Areejslama <Areegahmed449@gmail.com>
2024-03-26 11:19:14 -0700

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,10 +23,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->right = NULL;
 	if (parent == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
 	if (parent->left != NULL)
-	{	
+	{
 		new->left = parent->left;
 		parent->left->parent = new;
 	}
