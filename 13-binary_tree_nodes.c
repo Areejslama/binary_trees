f895833e61dfef54d8dfd19e6ea6451c83b19a07
Areejslama <Areegahmed449@gmail.com>
2024-03-26 17:53:11 -0700

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,15 +9,17 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t l_size;
-	size_t r_size;
+	size_t num = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	l_size = binary_tree_nodes(tree->left);
-	r_size = binary_tree_nodes(tree->right);
-
-	return ( 1 + l_size + r_size);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (1);
+	}
+	num += binary_tree_nodes(tree->left);
+	num += binary_tree_nodes(tree->right);
+	return (num);
 }
