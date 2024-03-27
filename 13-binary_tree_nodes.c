8f50dcb34d5834d0c47ea9205529e6e4e427b519
Areejslama <Areegahmed449@gmail.com>
2024-03-26 17:44:28 -0700

    nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes- count nodes of binary tree
+ * @tree:pointer to root
+ * Return:0
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t l_size;
+	size_t r_size;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	l_size = binary_tree_nodes(tree->left);
+	r_size = binary_tree_nodes(tree->right);
+
+	return ( 1 + l_size + r_size);
+}
