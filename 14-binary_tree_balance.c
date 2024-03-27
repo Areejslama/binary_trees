c9c25051877a049589d7180fe1938907afeac71d
Areejslama <Areegahmed449@gmail.com>
2024-03-27 12:41:54 -0700

    balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance- measures the balance factor
+ * @tree:pointer to root
+ * Return:0
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l_balance = 0;
+	int r_balance = 0;
+	
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	l_balance = binary_tree_balance(tree->left);
+	r_balance = binary_tree_balance(tree->right);
+	return (l_balance - r_balance);
+}
