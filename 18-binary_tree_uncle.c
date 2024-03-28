69f7bb59785ad27385470e9bdb98b3848f50a9c7
Areejslama <Areegahmed449@gmail.com>
2024-03-27 18:02:49 -0700

    uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * *binary_tree_uncle- check if nodes are siblings
+ * @node:pointer to parent
+ * Return:0
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	return (node->parent->parent->right);
+}
